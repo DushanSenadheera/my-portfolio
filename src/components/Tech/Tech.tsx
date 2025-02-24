2298f11f011b023bca8bef8099b4ee6e4932e423
Dushan Senadheera <89749333+DushanSenadheera@users.noreply.github.com>
2025-02-24 15:15:56 +0530

    Update Tech.tsx

--- src/components/Tech/Tech.tsx
+++ src/components/Tech/Tech.tsx
@@ -7,7 +7,6 @@ export default function Tech() {
         <h3 className="sub-heading">Tools & Technologies</h3>
 {/*       <Marquee> */}
       <img
-         align="center"
           src="https://skillicons.dev/icons?i=react,nodejs,expressjs,angular,nestjs,graphql,kafka,redis,mongodb,mysql,postgres,next,redux,sass,tailwind,ts,aws,docker,kubernetes,githubactions,linux&perline=50"
           alt="tech"
         />
