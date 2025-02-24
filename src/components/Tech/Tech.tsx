0b5835c21c5e7979f5871723a6cdfbc7b2526e37
Dushan Senadheera <89749333+DushanSenadheera@users.noreply.github.com>
2025-02-24 12:18:36 +0530

    Update Tech.tsx

--- src/components/Tech/Tech.tsx
+++ src/components/Tech/Tech.tsx
@@ -1,16 +1,17 @@
-import Marquee from "react-fast-marquee";
+// import Marquee from "react-fast-marquee";
 import "./Tech.scss";
 
 export default function Tech() {
   return (
     <div className="tech">
         <h3 className="sub-heading">Tools & Technologies</h3>
-      <Marquee>
+{/*       <Marquee> */}
       <img
+         align="center"
           src="https://skillicons.dev/icons?i=react,nodejs,expressjs,angular,nestjs,graphql,kafka,redis,mongodb,mysql,postgres,next,redux,sass,tailwind,ts,aws,docker,kubernetes,githubactions,linux&perline=50"
           alt="tech"
         />
-      </Marquee>
+{/*       </Marquee> */}
     </div>
   );
 }
